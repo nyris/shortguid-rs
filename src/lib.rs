982f1262d1b53ebe014aeccce05889af797abfbe
Markus Mayer <widemeadows@gmail.com>
2024-05-05 14:33:31 +0200

    Prepare Zerocopy support

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,9 @@ use std::fmt::{Debug, Display, Formatter};
 use std::str::FromStr;
 use uuid::Uuid;
 
+#[cfg(all(uuid_unstable, feature = "zerocopy"))]
+use zerocopy::{AsBytes, FromBytes, Unaligned};
+
 /// A short, URL-safe UUID representation.
 ///
 /// ## Example
@@ -52,10 +55,10 @@ use uuid::Uuid;
 /// ```
 #[derive(Default, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
-// #[cfg_attr(
-//      feature = "zerocopy",
-//      derive(zerocopy::AsBytes, zerocopy::FromBytes, zerocopy::Unaligned)
-// )]
+#[cfg_attr(
+    all(uuid_unstable, feature = "zerocopy"),
+    derive(AsBytes, FromBytes, Unaligned)
+)]
 #[cfg_attr(
     feature = "bytemuck",
     derive(bytemuck::Zeroable, bytemuck::Pod, bytemuck::TransparentWrapper)
